b6896325615dabe77a6050796ba41dca2229021e
Delany Schucker <dschucker@penguinrandomhouse.com>
2023-07-20 23:14:21 -0400

    refactor: update restart code for consistency

--- bikeshare.py
+++ bikeshare.py
@@ -226,8 +226,8 @@ def main():
         user_stats(df, city)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = input('\nWould you like to restart? Enter yes or no.\n').lower()
+        if restart != 'yes':
             break
 
 
